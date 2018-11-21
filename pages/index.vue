a263c4bdebbba99850e70a26e7c195594c553a7c
subuta <shunta.3110@gmail.com>
2018-11-21 16:42:00 +0900

    Add initial files.

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>TV Shows</h1>
+
+    <ul>
+      <li
+        v-for="show in shows"
+        :key="show.id"
+      >
+        <a :href="`/shows/${show.id}`">{{ show.name }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ app }) {
+    const shows = await app.$axios.$get('https://api.tvmaze.com/shows?page=0')
+    return { shows }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ pages/shows/_id.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>TV Show(ID: {{ show.id }})</h1>
+
+    <h3>{{ show.name }}</h3>
+    <div v-html="show.summary" />
+
+    <img :src="show.image.medium">
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ app, params }) {
+    const show = await app.$axios.$get(
+      `https://api.tvmaze.com/shows/${params.id}`
+    )
+    return { show }
+  }
+}
+</script>
+
+<style>
+</style>
